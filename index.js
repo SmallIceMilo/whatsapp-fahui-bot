0fc450ad6f763187cdc7f9c923fda82b70dd55d6
SmallIceMilo <jefflau1993@hotmail.com>
2026-03-11 11:37:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const serviceAccount = {
   type: 'service_account',
   project_id: process.env.GOOGLE_PROJECT_ID,
   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   client_email: process.env.GOOGLE_CLIENT_EMAIL,
   client_id: process.env.GOOGLE_CLIENT_ID
 };
@@ -659,3 +659,4 @@ ${message.body}`
 
 console.log('NEW CODE VERSION LOADED');
 client.initialize();
+
